Fix MCMC densficiation overflow

scene/gaussian_model.py
@@ -1813,12 +1813,7 @@ class GaussianModel:
         # Only Leaf nodes can be used for respawning 
         alive_indices=torch.where(self.nodes[:size, hierarchy_node_child_count] == 0)[0]
         
-        
-        
-        
         #probs = (self.opacity_activation(self._opacity[alive_indices, 0])) 
-        
-        
         
         if densification == "classic":
             #probs *= (self._densification_criterium[alive_indices] + 1)
@@ -1836,6 +1831,9 @@ class GaussianModel:
             add_idx, ratio = self._sample_alives(probs=probs, num=num_gs, alive_indices=alive_indices)
             # make sure respawn gaussians are unique
             add_idx = torch.where(ratio == 1)[0]
+            if (len(add_idx) * 2) + self.size > cap_max:
+                to_add = max(cap_max - self.size, 0) // 2
+                add_idx = add_idx[: to_add]
         ratio = torch.zeros((self.size, 1), device=device, dtype=torch.int32)
         ratio[add_idx] = 1
         (   new_xyz, 

train_coarse.py
@@ -31,7 +31,7 @@ def training(dataset, opt, pipe, saving_iterations, checkpoint_iterations, check
     prepare_output_and_logger(dataset)
     print("coarse source path: " + dataset.source_path)
     gaussians = GaussianModel(1)
-    scene = Scene(dataset, gaussians)
+    scene = Scene(dataset, gaussians, opt.llff_hold)
     with torch.no_grad():
         gaussians._opacity[:] = -3
     gaussians.training_setup(opt)